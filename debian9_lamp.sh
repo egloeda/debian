235be5e2c523c7a9d28fa430fd6d397f98c654f4
Emilio Gª Loeda <emilio.loeda@gmail.com>
2017-11-26 21:07:06 +0100

    Update debian9_lamp.sh

--- debian9_lamp.sh
+++ debian9_lamp.sh
@@ -1,2 +1,3 @@
 apt-get -y install apache2-ssl-dev libapache2-mod-php php mariadb-server
+apt-get -y install php-gd php-readline php-mbstring php-curl
 echo "<?php phpinfo(); ?>" > /var/www/html/p1.php
