05fe5ea6fcb8f9a935e557f3b82fff302e4be97a
Emilio Gª Loeda <emilio.loeda@gmail.com>
2017-11-26 22:29:14 +0100

    Update debian9_lamp.sh

--- debian9_lamp.sh
+++ debian9_lamp.sh
@@ -4,6 +4,7 @@ apt-get -y install postfix
 echo "<?php phpinfo(); ?>" > /var/www/html/p1.php
 
 mkdir -p /software/{mantisbt,dokuwiki}
+mkdir -p /var/www/html/{prod,test}/{mantis,faq}
 cd /sofrware/mantisbt
 wget https://sourceforge.net/projects/mantisbt/files/mantis-stable/2.8.0/mantisbt-2.8.0.tar.gz/download
 mv download mantisbt-2.8.0.tar.g
