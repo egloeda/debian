7620feb645ec0d6194ebe7b28bc6528f53dc936f
Emilio Gª Loeda <emilio.loeda@gmail.com>
2017-11-27 09:53:59 +0100

    Update debian9_lamp.sh

--- debian9_lamp.sh
+++ debian9_lamp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sh -c "$(wget https://raw.githubusercontent.com/egloeda/debian/master/debian9_lamp.sh -O -)"
+# sh -c "$(wget https://raw.githubusercontent.com/egloeda/debian/master/debian9_lamp.sh -O -)"
 
 apt-get -y install apache2-ssl-dev libapache2-mod-php php mariadb-server
 apt-get -y install php-gd php-readline php-mbstring php-curl php-mysql
